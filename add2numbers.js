e0162604b594c21a1c1c3f55671c1812a41014e0
bhanu9505508737 <45350818+bhanu9505508737@users.noreply.github.com>
2018-12-05 17:21:51 +0530

    adds node js

---
+++ add2numbers.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    var y = 1;
+    var z = 2;
+    var x = y+z;
+    console.log(x);
+  }myFunction();
---
+++ highestnumber.js
@@ -0,0 +1,6 @@
+function myFunction() {
+
+    var b = Math.max(0, 150, 30, 20, 38);
+    console.log(b);
+}myFunction();
+    
